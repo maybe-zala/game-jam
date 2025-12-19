7261fa9f824bb6e994ce9d767b1286d14e5a3a04
possibly-nai <nfarr26@basecampcodingacademy.org>
2025-12-18 21:58:15 -0600

    lalalala all done lalala

---
+++ notesrq.md
@@ -0,0 +1,83 @@
+12/5 - Started Project 
+
+Came up with idea
+
+Practiced pixel art design 
+
+pixel art is so hard what ajfoiajoiejfa
+
+12/8 - Made idle animation sprite sheet 
+
+What does a sunflower even look like
+
+Made excited sprite sheet 
+
+Remade backgrounds(going to do again for final product) 
+
+Followed tutorial to make idle animation work with: 
+
+https://www.youtube.com/watch?v=M6e3_8LHc7A 
+
+ https://www.youtube.com/watch?v=nXOVcOBqFwM 
+
+it so hard whataiojfoafpaif omg ajojriofjaf
+
+12/9 - combined all sunflower sprite sheets 
+
+              (talking 1-4, excited 5-11, idle 12-25, evolve 26-45, new idle 46-56, new excited 57-64, new talking 65-69, old talking 70-73, old excited 74-82, old idle 83-97, death 104-116)  
+
+Finished tutorial to make animations change: 
+
+https://www.youtube.com/watch?v=nXOVcOBqFwM 
+
+Updated backgrounds to fit artstyle 
+
+Are we all perhaps just sunflowers in a field, turning our head to the light to avoid the dark
+
+Created watering can sprite sheet 
+
+Started homescreen 
+
+Set up Space to start game 
+
+12/10 - Created fertilizer sprite and integrated it 
+
+Browsed several tutorials about how to get the stupid text box 
+
+Redesigned buttons and made them very cute 😊 
+
+Other misc junk 
+
+12/11 - Started evolving animation 
+
+Not much else had work to do :( 
+
+12/15 - we are so back finished my assignment so i can work on this 
+
+	Made evolving animation 
+
+	Made new evolved flower idle and excited and talking. 
+
+12/16 - working so very hard to clean up new animations. Might redo the backgrounds idk yet tho. 
+
+Made death animation and updated to implement them all. 
+
+Added paint feature BIG YIPEE ill link the tuts later if i remember 
+
+Changed day cycle to three clicks per time 
+
+Changed paint to only happen for each life stage of flower 
+
+12/17 - fixed death animation looping with old flower on wilt(frames were wrong :p)
+
+12/18 - finished up and checked for bugs because im good like that
+
+        changed all the phrases because they were stupid (they still are but its not as bad)
+
+        about to submit i locked in and im so very tired
+
+        deleted all my nonsense comments i wrote at 5am because they don't make sense anymore an half of them are wrong now.
+
+first pygame project kind of nervous
+
+SHOUTOUT TO CODING WITH RUSS I LOVE YOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU MY LIFE IS YOURS!!!!!!!! EVERYONE FOLLOW HIM IMMEDIATELY AND LIKE ALL HIS VIDS WOOOOOOOOOO
--- sprites.py
+++ sprites.py
@@ -6,18 +6,78 @@ def load_sunflower_animations():
     sunflower = pygame.image.load("spritesheets/sunflower.png").convert_alpha()
     sprite_sheet = spritesheet.SpriteSheet(sunflower)
     
-    animation_list = []
-    animation_steps = [4, 7, 14]
+    # Base animations: talking (1-4), excited (5-11), idle (12-25)
+    base_animations = []
+    base_steps = [4, 7, 14]  # frames for each animation
     step_counter = 0
-    
-    for animation in animation_steps:
+    # (talking 1-4, excited 5-11, idle 12-25, evolve 26-45, new idle 46-56, new excited 57-64, new talking 65-69) 
+    for animation in base_steps:
         temp_img_list = []
         for _ in range(animation):
             temp_img_list.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
             step_counter += 1
-        animation_list.append(temp_img_list)
+        base_animations.append(temp_img_list)
     
-    return animation_list
+    # Evolve animation (26-45): 20 frames
+    evolve_animation = []
+    for _ in range(20):
+        evolve_animation.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Evolved animations: new talking (65-69), new excited (57-64), new idle (46-56)
+    # Need to load in order: talking, excited, idle to match action indices 0, 1, 2
+    evolved_animations = []
+    
+    # First load new idle (46-56): 11 frames
+    new_idle = []
+    for _ in range(11):
+        new_idle.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Then load new excited (57-64): 8 frames
+    new_excited = []
+    for _ in range(8):
+        new_excited.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Finally load new talking (65-69): 5 frames
+    new_talking = []
+    for _ in range(5):
+        new_talking.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Reorder to match action indices: [0: talking, 1: excited, 2: idle]
+    evolved_animations = [new_talking, new_excited, new_idle]
+    
+    # Old animations: old talking (70-73), old excited (74-82), old idle (83-97)
+    # Load old talking (70-73): 4 frames
+    old_talking = []
+    for _ in range(4):
+        old_talking.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Load old excited (74-82): 9 frames
+    old_excited = []
+    for _ in range(9):
+        old_excited.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Load old idle (83-103): 21 frames
+    old_idle = []
+    for _ in range(21):
+        old_idle.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    # Order: [0: talking, 1: excited, 2: idle]
+    old_animations = [old_talking, old_excited, old_idle]
+    
+    # Death animation (104-116): 13 frames
+    death_animation = []
+    for _ in range(13):
+        death_animation.append(sprite_sheet.get_image(step_counter, 50, 100, 3, (0, 0, 0)))
+        step_counter += 1
+    
+    return base_animations, evolve_animation, evolved_animations, old_animations, death_animation
 
 def load_watering_can_animation():
     watering_can = pygame.image.load("spritesheets/watering can.png").convert_alpha()
